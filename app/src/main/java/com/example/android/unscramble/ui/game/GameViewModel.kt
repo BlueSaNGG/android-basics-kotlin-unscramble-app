6f1dd47820fc9c6374d3a4fdb82a34c0ba2eb7ff
qjin1 <qjin1@paypal.com>
2023-03-28 17:50:52 -0500

    add a dialog
    Add the dialog

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -3,11 +3,15 @@ package com.example.android.unscramble.ui.game
 import android.util.Log
 import androidx.lifecycle.ViewModel
 
-class GameViewModel: ViewModel() {
-    private var score = 0
+class GameViewModel : ViewModel() {
+    private var _score = 0
+    val score: Int
+        get() = _score
+
     private var _currentWordCount = 0
     val currentWordCount: Int
         get() = _currentWordCount
+
     private lateinit var _currentScrambledWord: String
     val currentScrambledWord: String
         get() = _currentScrambledWord
